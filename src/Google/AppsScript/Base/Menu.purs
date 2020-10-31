0e3b5670bde29a05102645c71d7640de1755644b
Alex Kelley <https://github.com/adkelley>
2020-10-31 16:03:44 -0700

    moved Menu and Ui to Base

--- src/Google/AppsScript/Base/Menu.purs
+++ src/Google/AppsScript/Base/Menu.purs
@@ -1,8 +1,8 @@
 module Google.AppsScript.Base.Menu
 ( addItem
-, addToUi
 , addSeparator
 , addSubMenu
+, addToUi
 ) where
 
 import Prelude (Unit)
@@ -12,9 +12,17 @@ import Google.AppsScript.Base.Types (Menu)
 
 
 foreign import addItemImpl::Fn3 String String Menu (GASEff Menu)
+
+-- | Inserts the menu into the instance of the editor's user interface.
 foreign import addToUi::Menu -> GASEff Unit
+
+-- | Adds a visual separator to the menu.
 foreign import addSeparator::Menu -> GASEff Menu
+
+-- | Adds a sub-menu to the menu.
 foreign import addSubMenu::Menu -> Menu -> GASEff Menu
 
+-- | Adds an item to the menu. The label for a menu item should be in sentence 
+-- | case (only the first word capitalized).
 addItem::String -> String -> Menu -> GASEff Menu
 addItem cap funcname menu = runFn3 addItemImpl cap funcname menu
--- src/Google/AppsScript/Base/Ui.purs
+++ src/Google/AppsScript/Base/Ui.purs
@@ -10,49 +10,65 @@ module Google.AppsScript.Base.Ui
 , showModelessDialog
   {-
   , prompt
-  , createAddonMenu
   -}
 ) where
 
 import Prelude
-import Data.Function.Uncurried (Fn2, Fn5, Fn4, Fn3,
-                                runFn2, runFn5, runFn4, runFn3)
+
+import Data.Function.Uncurried (Fn2, Fn5, Fn4, Fn3, runFn2, runFn5, runFn4, runFn3)
 import Google.AppsScript.AppsScript (GASEff)
-import Google.AppsScript.Html (HtmlOutput)
 import Google.AppsScript.Base.Types (Ui, Menu)
+import Google.AppsScript.Html (HtmlOutput)
 
 
+-- | An enum representing predetermined, localized dialog buttons returned by 
+-- | an alert or PromptResponse.getSelectedButton() to indicate which button 
+-- | in a dialog the user clicked.
 data Button = Close | Ok | Cancel | Yes | No
+
+-- | An enum representing predetermined, localized sets of one or more dialog 
+-- | buttons that can be added to an alert or a prompt.
 data ButtonSet = OkAlone | OkCancel | YesNo | YesNoCancel
 
-foreign import alertImpl::Fn3      (Array Button)
-                                   String Ui (GASEff Button)
-foreign import alertBtnsImpl::Fn4  (Array Button)
-                                   String ButtonSet Ui (GASEff Button)
-foreign import alertTitleImpl::Fn5 (Array Button)
-                                   String String ButtonSet Ui (GASEff Button)
-foreign import createAddonMenu::Ui -> GASEff Menu
-foreign import showSidebarImpl::Fn2 HtmlOutput Ui (GASEff Unit)
-foreign import showModalDialogImpl::Fn3 HtmlOutput String Ui (GASEff Unit)
-foreign import showModelessDialogImpl::Fn3 HtmlOutput String Ui (GASEff Unit)
+foreign import alertImpl :: Fn3 (Array Button) String Ui (GASEff Button)
+foreign import alertBtnsImpl :: Fn4 (Array Button) String ButtonSet Ui (GASEff Button)
+foreign import alertTitleImpl :: Fn5 (Array Button) String String ButtonSet Ui (GASEff Button)
 
-btns::Array Button
+-- | Creates a builder that can be used to insert a sub-menu into the editor's 
+-- | Add-on menu.
+foreign import createAddonMenu :: Ui -> GASEff Menu
+
+foreign import showSidebarImpl :: Fn2 HtmlOutput Ui (GASEff Unit)
+foreign import showModalDialogImpl :: Fn3 HtmlOutput String Ui (GASEff Unit)
+foreign import showModelessDialogImpl :: Fn3 HtmlOutput String Ui (GASEff Unit)
+
+btns :: Array Button
 btns = [Close, Ok, Cancel, Yes, No]
 
-alert::String -> Ui -> GASEff Button
+-- | Opens a dialog box in the user's editor with the given message and an "OK" 
+-- | button.
+alert :: String -> Ui -> GASEff Button
 alert msg ui = runFn3 alertImpl btns msg ui
 
-alertButtons::String -> ButtonSet -> Ui -> GASEff Button
+-- | Opens a dialog box in the user's editor with the given message and set of 
+-- | buttons.
+alertButtons :: String -> ButtonSet -> Ui -> GASEff Button
 alertButtons msg btnset ui = runFn4 alertBtnsImpl btns msg btnset ui
 
-alertTitle::String -> String -> ButtonSet -> Ui -> GASEff Button
+-- | Opens a dialog box in the user's editor with the given title, message, and 
+-- | set of buttons.
+alertTitle :: String -> String -> ButtonSet -> Ui -> GASEff Button
 alertTitle title msg btnset ui = runFn5 alertTitleImpl btns title msg btnset ui
 
-showSidebar::HtmlOutput -> Ui -> GASEff Unit
+-- | Opens a sidebar in the user's editor with custom client-side content.
+showSidebar :: HtmlOutput -> Ui -> GASEff Unit
 showSidebar html ui = runFn2 showSidebarImpl html ui
 
-showModalDialog::HtmlOutput -> String -> Ui -> GASEff Unit
+-- | Opens a modal dialog box in the user's editor with custom client-side content.
+showModalDialog :: HtmlOutput -> String -> Ui -> GASEff Unit
 showModalDialog html title ui = runFn3 showModalDialogImpl html title ui
 
-showModelessDialog::HtmlOutput -> String -> Ui -> GASEff Unit
+-- | Opens a modeless dialog box in the user's editor with custom client-side 
+-- | content.
+showModelessDialog :: HtmlOutput -> String -> Ui -> GASEff Unit
 showModelessDialog html title ui = runFn3 showModelessDialogImpl html title ui
